81b60b7c882631a3ebce1ce83396c31095de3b11
danielacevedo20 <acevedo.jose@uces.edu.co>
2021-03-11 07:48:29 -0500

    Clase 11/03/2021

--- Clases/listas.py
+++ Clases/listas.py
@@ -1,16 +1,16 @@
 nombres = []
 print (type(nombres))
 print (nombres)
-nombres = ["Santi","Aleja","Elsa"]
+nombres = ["Santi","Aleja","Elsa","Samuek"]
 print (nombres)
-print (nombres[1])
+print (nombres[2])
 nombres.append ("Mauricio")
 print (nombres)
 
-edades = [18,19,20,17]
+edades = [18,19,20,17,32,14,13,12,15,16]
 estaturas = [1.62,1.80,1.67,1.98]
 #Mostrar el último valor de la lista
-print (edades[-1])
+print (edades[-2])
 print (edades[0:2])
 print (edades[:3])
 print (edades[2:])
@@ -55,8 +55,15 @@ for posicion in range(largolista):
 print (edades)
 print (posicionesConValoresPares)
 #solo cuando les interese mostrar la lista
+posicion=0    
 for edad in edades:
     print(edad)
+for nombre in nombres:
+    print
+    print(nombre)
+    print(posicion)
+    posicion+=1
+
 
 
 posicion=0
@@ -64,3 +71,5 @@ posicionesPares = []
 for edad in edades:
     if (edad%2==0):
         posicionesPares.append(posicion)
+    posicion+=1
+print(posicionesPares)
