b4b4050cda84f7b30b64552a2b03ebe92536423c
danielacevedo20 <acevedo.jose@uces.edu.co>
2021-04-13 07:18:13 -0500

    Clase del 13/04/2021

---
+++ Clases/clasesyobjetos/clases1.py
@@ -0,0 +1,44 @@
+class Humano ():
+    '''Mensaje que define a la clase
+
+
+
+    '''
+    def __init__ (self, nombreEntrada, edadEntrada, entradaEstatura):
+        print ("Hola soy humano")
+        self.edad = (edadEntrada)
+        self.raza = "Humano"
+        self.nombre = nombreEntrada
+        self.estatura = entradaEstatura
+        self.dinero = 0
+    def hablar (self, mensaje):
+        print (f"Hola soy {self.nombre} y tengo un mensaje que decir...", mensaje)
+    def mostrarAtributos (self):
+        print (f'''Mi nombre es {self.nombre}
+        Mi edad es {self.edad}
+        Mi estatura es {self.estatura}''')
+    def recorrerDistancia (self, distanciaMetros):
+        for i in range (distanciaMetros):
+            print (f"Hola soy {self.nombre} y he recorrido {i+1} metros")
+    def ahorrarDinero (self):
+        preguntaIngresarMontos ='''S para continuar añadiendo montos
+        N para finalizar: '''
+        preguntaMonto = "Cuanto vas a ingresar: "
+        ingresarMontos = input(preguntaIngresarMontos)
+        while(ingresarMontos != "N"):
+            monto = float(input(preguntaMonto))
+            self.dinero = self.dinero + monto
+            print (f"Llevas {self.dinero} ahorrado")
+            ingresarMontos = input(preguntaIngresarMontos)
+        return self.dinero
+
+
+
+
+humano1 = Humano("Daniel",18,1.83)
+
+humano1.hablar("Espero que estén muy bien")
+humano1.mostrarAtributos ()
+humano1.recorrerDistancia(5)
+totalAhorrado = humano1.ahorrarDinero()
+print (totalAhorrado)
